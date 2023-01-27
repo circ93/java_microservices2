402795c967eedd40f4f956ff277f1d274a4b6d59
Gianluca <circelli93@gmail.com>
2023-01-27 16:11:19 +0100

    esercizio 9

---
+++ Esercizio9/PizzaService/src/main/java/it/corso/pizzaservice/controller/PizzaController.java
@@ -0,0 +1,37 @@
+package it.corso.pizzaservice.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import it.corso.pizzaservice.dto.PizzaDTO;
+import it.corso.pizzaservice.dto.RestaurantIdsDTO;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Api(tags = "Pizza API")
+public interface PizzaController {
+
+    //definizamo il metodo in swagger
+    @ApiOperation("Add new pizza")
+    public PizzaDTO save(@RequestBody PizzaDTO pizzaDTO);
+
+    @ApiOperation("Find pizza by ID")
+    public PizzaDTO findById(@PathVariable("id") Long id);
+
+    @ApiOperation("Delete by ID")
+    public void deleteById(@PathVariable("id") Long id);
+
+    @ApiOperation("Find all pizzas")
+    public List<PizzaDTO> findAll();
+
+    @ApiOperation("Update pizza")
+    public PizzaDTO update(@RequestBody PizzaDTO pizzaDTO, @PathVariable("id") Long id);
+
+    @ApiOperation("Find pizzas by restaurants id")
+    public List<PizzaDTO> findByRestaurantId(@PathVariable("id") Long id);
+
+    @ApiOperation("Add pizza to restaurant")
+    public List<PizzaDTO> addPizzasToRestaurant(@RequestBody List<RestaurantIdsDTO> restaurantIdsDTOS);
+
+}
